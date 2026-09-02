2323875cb1c2e83ef41bdba11739716c590e5248
DiegoAbraham15 <diegoabraham061@gmail.com>
2026-09-02 11:14:20 -0600

    Final

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Iniciar sesión | QUANTIKA POOL</title>
+    <title>Iniciar sesión </title>
     <link rel="icon" href="{{ asset('favicon.ico') }}">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
